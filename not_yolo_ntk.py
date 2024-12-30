3ca824e2d4ad5d3d577a0b7ca7193517b56f5b1d
stilxam <m.l.h.litsios@student.tue.nl>
2024-12-30 14:26:48 +0100

    it works with wire????

--- not_yolo_ntk.py
+++ not_yolo_ntk.py
@@ -1,9 +1,10 @@
+from jax.nn import initializers
 import numpy as np
 import jax
 import flax
 import optax
 import neural_tangents as nt
-from typing import Callable, Sequence, Any, Optional
+from typing import Callable, Sequence, Any, Optional, Union
 import flax.linen as nn
 import jax.numpy as jnp
 from jax import random, jit, vmap
@@ -11,12 +12,6 @@ from inr_utils.images import make_lin_grid
 from common_jax_utils import key_generator
 
 key_gen = key_generator(random.PRNGKey(0))
-
-
-
-    
-
-
 
 # MLP
 class MLP(nn.Module):
@@ -102,9 +97,7 @@ class SIREN(nn.Module):
         # final layer
         if self.outermost_linear:
             x = nn.Dense(
-                self.features[-1],
-                kernel_init=my_uniform(scale=jnp.sqrt(6 / feat_in) / self.hidden_omega_0),
-                bias_init=my_uniform(scale=jnp.sqrt(6 / feat_in) / self.hidden_omega_0),
+                self.features[-1], kernel_init=my_uniform(scale=jnp.sqrt(6 / feat_in) / self.hidden_omega_0), bias_init=my_uniform(scale=jnp.sqrt(6 / feat_in) / self.hidden_omega_0),
             )(x)
         else:
             x = self.activation(
@@ -124,6 +117,131 @@ def my_uniform(scale=1e-2, dtype=jnp.float32):
         return random.uniform(key, shape, dtype, -1, 1) * scale
 
     return init
+
+
+def complex_gabor_wavelet(*x: jax.Array, s0:Union[float, jax.Array], w0: Union[float, jax.Array])->jax.Array:
+    """ 
+    Implements the n-dimensional WIRE activation function as per the 2D extension.
+    :parameter x: one jax.Array per dimension
+        should all have the same size
+    :parameter s0: inverse scale parameter of the wavelet (s_0 in the paper)
+    :parameter w0: frequency scale parameter of the wavelet (\omega_0 in the paper)
+    :returns: the computed wavelet as a JAX array
+    """
+    # Frequency modulation ( w0 applies only to the first coordinate)
+    freq = jnp.exp(1j * w0 * x[0])
+    # Radial part: Gaussian envelope
+    x = jnp.stack(x, axis=0)
+    squared_abs = jnp.mean(jnp.square(jnp.abs(x)), axis=0)  # mean instead of sum so that we don't have to change the initialization scheme
+    gaussian_envelope = jnp.exp(-jnp.square(s0)*squared_abs)
+    return freq * gaussian_envelope
+
+
+class RealWIREInitializer():  # Subclass from _Initializer
+    def __init__(self) -> None:
+        pass
+
+
+    def __call__(self, key, shape: Sequence[int], dtype=jnp.float32) -> jnp.ndarray:
+        in_size = np.prod(shape[:-1])  # For simplicity we assume last dimension is out size
+        lim = jnp.sqrt(1./in_size)
+        real_part = jax.random.uniform(next(key_gen),shape, minval=-lim, maxval=lim)
+        return real_part 
+
+class BiasRealWIREInitializer():
+    def __init__(self) -> None:
+        pass
+    def __call__(self, key, shape: Sequence[int], dtype=jnp.float32) -> jnp.ndarray:
+        in_size = np.prod(shape[:-1])  # For simplicity we assume last dimension is out size
+        real_part = jax.random.uniform(next(key_gen),shape, minval=-1, maxval=1)
+        return real_part
+
+
+class ComplexWIREInitializer():  # Subclass from _Initializer
+    def __init__(self, w0: float):
+        self.w0 = w0
+
+    def __call__(self, key, shape: Sequence[int], dtype=jnp.complex64) -> jnp.ndarray:
+        in_size = np.prod(shape[:-1])  # For simplicity we assume last dimension is out size
+        lim = jnp.sqrt(6./in_size)/self.w0
+        real_part = jax.random.uniform(next(key_gen),shape, minval=-lim, maxval=lim)
+        imag_part = jax.random.uniform(next(key_gen), shape, minval=-lim, maxval=lim)
+        return real_part + 1j * imag_part
+
+
+class BiasComplexWIREInitializer():  # Subclass from _Initializer
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self, key, shape: Sequence[int], dtype=jnp.complex64) -> jnp.ndarray:
+        in_size = np.prod(shape[:-1])  # For simplicity we assume last dimension is out size
+        lim = jnp.sqrt(2)
+        real_part = jax.random.uniform(next(key_gen),shape, minval=-lim, maxval=lim)
+        imag_part = jax.random.uniform(next(key_gen), shape, minval=-lim, maxval=lim)
+        return real_part + 1j * imag_part
+
+
+class WIRE(nn.Module):
+    features: Sequence[int]
+    w0: float = 30
+    s0: float = 15
+    activation: Callable = complex_gabor_wavelet
+    input_dim: int = 1
+    outermost_linear: bool = True
+
+    @nn.compact
+    def __call__(self, x):
+
+        # first layer initialization is different than others
+        feat_in = self.input_dim
+        x = self.activation(
+            nn.Dense(
+                self.features[0],
+                kernel_init=RealWIREInitializer(),
+                bias_init=BiasRealWIREInitializer(),
+            )(x),
+            s0=self.s0,
+            w0=self.w0
+
+        )
+
+        # rest of the layers
+        feat_in = self.features[0]
+        for feat in self.features[1:-1]:
+            x = self.activation(
+                 nn.Dense(
+                    feat,
+                    kernel_init=ComplexWIREInitializer(self.w0),
+                    bias_init=BiasComplexWIREInitializer()
+                )(x),
+                s0=self.s0,
+                w0=self.w0
+
+            )
+            feat_in = feat
+
+        # final layer
+        if self.outermost_linear:
+            x = nn.Dense(
+                    self.features[-1],
+                    kernel_init=ComplexWIREInitializer(self.w0),
+                    bias_init=BiasComplexWIREInitializer()
+            )(x)
+
+
+        else:
+            x = self.activation(
+                nn.Dense(
+                    self.features[-1],
+                    kernel_init=ComplexWIREInitializer(self.w0),
+                    bias_init=BiasComplexWIREInitializer()
+                )(x),
+                s0=self.s0,
+                w0=self.w0)
+
+        return jnp.real(x)
+
+
 
 
 def get_ntk_fns(model, xs):
@@ -229,5 +347,35 @@ def use_ntk_to_predict():
 
 
 
+
+
+
 if __name__ == "__main__":
-    use_ntk_to_predict()
+    #initializer = nn.initializers.xavier_uniform()(next(key_gen), (2,3), jnp.complex64)
+    #print(f"{initializer=}")
+    #print(f"{initializer.shape=}")
+    #print(f"{initializer.dtype=}")
+    flattened_locations, in_channels, rgb_vals, out_channels = prep_x_ys()
+    
+    model = WIRE(
+        w0=20,
+        s0=15,
+        features=[2,3,3,1]
+    )
+
+    params = model.init(random.PRNGKey(0), flattened_locations)
+    params, (ntk_fn_jacobian_contraction, ntk_fn_ntvp, ntk_fn_str_derivatives, ntk_fn_auto) = get_ntk_fns(model = model, xs=flattened_locations)
+
+    k_1 = ntk_fn_jacobian_contraction(flattened_locations, flattened_locations, params)
+    k_2 = ntk_fn_ntvp(flattened_locations, flattened_locations, params)
+    k_3 = ntk_fn_str_derivatives(flattened_locations, flattened_locations, params)
+    k_4 = ntk_fn_auto(flattened_locations, flattened_locations, params)
+
+    print(f"{k_1.shape=}")
+    print(f"{k_2.shape=}")
+    print(f"{k_3.shape=}")
+    print(f"{k_4.shape=}")
+    
+    
+
+
