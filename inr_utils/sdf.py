ba9b087e6af769733601d8adaeb1fe5f87c50c22
ovindar <m.l.h.litsios@student.tue.nl>
2025-02-04 13:59:01 +0100

    normalized the normals

--- inr_utils/sdf.py
+++ inr_utils/sdf.py
@@ -15,23 +15,22 @@ class SDFDataLoader:
     def __init__(self, sdf_name: str, batch_size: int, keep_aspect_ratio: bool, key: jax.Array):
         self._cpu = jax.devices('cpu')[0]
         self._gpu = jax.devices('gpu')[0]
-        
+
         self.sdf_name = sdf_name
         self.batch_size = batch_size
         self.on_surface_count = batch_size // 2
         self.off_surface_count = batch_size - self.on_surface_count
         self.keep_aspect_ratio = keep_aspect_ratio
-        
+
         self.initial_key = jax.device_put(key, self._cpu)
-        
+
         fp_ply = Path.cwd().joinpath("example_data", f"{self.sdf_name}.ply")
 
         self.mesh = trimesh.load(str(fp_ply))
-        
+
         if not self.mesh.is_watertight:
             raise ValueError(f"Mesh {self.sdf_name} is not watertight")
-        
-        
+
         vertices = self.mesh.vertices
 
         if self.keep_aspect_ratio:
@@ -40,24 +39,24 @@ class SDFDataLoader:
         else:
             coord_max = np.amax(vertices, axis=0, keepdims=True)
             coords_min = np.amin(vertices, axis=0, keepdims=True)
-        
+
         vertices = (vertices - coords_min) / (coord_max - coords_min)
         vertices -= 0.5
         vertices *= 2
-        
+
         # Update the mesh vertices
         self.mesh.vertices = vertices
 
         if not self.mesh.is_watertight:
-            raise ValueError(f"Rescaled mesh {self.sdf_name} is not watertight, please fix the mesh before using this class")
-        
+            raise ValueError(
+                f"Rescaled mesh {self.sdf_name} is not watertight, please fix the mesh before using this class")
 
         coords = np.array(self.mesh.vertices)
         normals = np.array(self.mesh.vertex_normals)
 
         self.coords = jax.device_put(coords, self._cpu)
         self.normals = jax.device_put(normals, self._cpu)
-
+        self.normals = self.normals / jnp.linalg.norm(self.normals, axis=-1, keepdims=True)
 
     def __iter__(self):
         with jax.default_device(self._cpu):
@@ -102,7 +101,6 @@ class SDFDataLoader:
         """
         return jax.numpy.asarray(self.mesh.contains(coords))
 
-
     def get_sdf(self, coords: jax.Array) -> jax.Array:
         """
         Evaluates the mesh as an SDF function
@@ -113,64 +111,3 @@ class SDFDataLoader:
         return jax.numpy.asarray(trimesh.proximity.signed_distance(self.mesh, coords))
 
 
-# class target_SDF(Module):
-#     def __init__(self, sdf_name: str, batch_size: int, keep_aspect_ratio: bool, key: jax.Array):
-#         self._cpu = jax.devices('cpu')[0]
-#         self._gpu = jax.devices('gpu')[0]
-
-#         self.sdf_name = sdf_name
-#         self.batch_size = batch_size
-#         self.on_surface_count = batch_size // 2
-#         self.off_surface_count = batch_size - self.on_surface_count
-#         self.keep_aspect_ratio = keep_aspect_ratio
-
-#         self.initial_key = jax.device_put(key, self._cpu)
-
-#         fp_ply = Path.cwd().joinpath("example_data", f"{self.sdf_name}.ply")
-
-#         self.mesh = trimesh.load(str(fp_ply))
-
-#         if not self.mesh.is_watertight:
-#             raise ValueError(f"Mesh {self.sdf_name} is not watertight")
-
-#         vertices = self.mesh.vertices
-
-#         if self.keep_aspect_ratio:
-#             coord_max = np.amax(vertices)
-#             coords_min = np.amin(vertices)
-#         else:
-#             coord_max = np.amax(vertices, axis=0, keepdims=True)
-#             coords_min = np.amin(vertices, axis=0, keepdims=True)
-
-#         vertices = (vertices - coords_min) / (coord_max - coords_min)
-#         vertices -= 0.5
-#         vertices *= 2
-
-#         # Update the mesh vertices
-#         self.mesh.vertices = vertices
-
-#         if not self.mesh.is_watertight:
-#             raise ValueError(
-#                 f"Rescaled mesh {self.sdf_name} is not watertight, please fix the mesh before using this class")
-
-#         coords = np.array(self.mesh.vertices)
-#         normals = np.array(self.mesh.vertex_normals)
-
-#         self.coords = jax.device_put(coords, self._cpu)
-#         self.normals = jax.device_put(normals, self._cpu)
-
-#     def __call__(self, coords: jax.Array) -> jax.Array:
-#         """
-#         Evaluates the mesh as an occupancy function
-
-#         :parameter coords: the coordinates to evaluate the occupancy function at shape (N, 3)
-#         :return: the occupancy function values at the coordinates shape (N, 1)
-#         """
-#         return jax.numpy.array(self.mesh.contains(coords))
-
-
-# if __name__ == "__main__":
-#     key = jax.random.PRNGKey(42)
-#     coords = jax.random.uniform(key, shape=(100, 3), minval=-1., maxval=1.)
-#     target_sdf = target_SDF("Armadillo", 100, True, key)
-#     print(target_sdf(coords).shape)
